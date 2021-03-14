319c20dad3e62924149293e2e1d1d133ea6493ef
ehdrjs5161 <ehdrjs5161@gmail.com>
2021-03-14 21:14:10 +0900

    2021.03.14

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask, jsonify
+from flask import Flask
 import pandas as pd
-import json
-from collections import OrderedDict
-from pythonCode import company, method
+from pythonCode import company, method, DB_Handler
 import os
+import datetime
+from collections import OrderedDict
+mongo = DB_Handler.DBHandler()
 app = Flask(__name__, static_folder='pythonCode')
 app.config['JSON_AS_ASCII'] = False
+
+
+@app.route('/code', methods=['GET', 'POST'])
+def code():
+    code_list = mongo.find
+
 
 @app.route('/api', methods=['GET'])
 def api():
@@ -17,34 +24,36 @@ def api():
 
 @app.route('/code/<code>', methods=['GET', 'POST'])
 def predict(code):
-    file_path = "./json_result/withNews/" + code + ".json"
-    # if not os.path.isfile(./json_result/"+code):
-    if not os.path.isfile("./json_result/withNews/"+code+".json"):
-        kospi = pd.read_csv("file/KOSPI200.csv")[['종목코드', '기업명']]
-        name = method.code_to_name(kospi, code)
-        comp = company.companys(name=name, code=code)
-        comp.load_data()
-        # comp.update_data()
-        comp.model_setting(10, 28, 3)
-        # comp.predict_price_day1()
-        # comp.predict_price_day7()
-        # comp.result_save()
+    kospi = mongo.find_items(db_name="stockPredict", collection_name="code")
+    kospi = pd.DataFrame(kospi)[['종목코드', '기업명']]
+    name = method.code_to_name(kospi, code)
+    comp = company.companys(name=name, code=code)
+    comp.load_data()
+    update = method.date_to_str(datetime.datetime.today() - datetime.timedelta(days=1))
+    print(update, comp.update_day)
 
-    with open(file_path, "r") as json_file:
-        result = json.load(json_file)
-    return result["{}".format(code)]
+    if update != comp.update_day:
+        comp.update_data()
+        comp.model_setting(10, 28, 3)
+        comp.predict_price_day1()
+        comp.predict_price_day7()
+        comp.result_save()
+
+    result = mongo.find_item(condition={"code": "{}".format(comp.code)}, db_name="stockPredict", collection_name="predictResult")
+    del result['_id']
+
+    return result
 
 @app.route('/rank', methods=['GET', 'POST'])
 def rank():
-    file_path = "json_result/withNews/Rank.json"
-    if os.path.isfile(file_path):
-        with open(file_path, "r") as json_file:
-            result = json.load(json_file)
-
-        return result
-
+    cursor = mongo.find_items(db_name="stockPredict", collection_name="rank")
+    result = OrderedDict()
+    result_list=[]
+    for rank in cursor:
+        del rank['_id']
+        result_list.append(rank)
+    result['result'] = result_list
+    return result
 
 if __name__ == "__main__":
-    result = predict("068270")
-    # result = rank()
-    # print(result)
+    print(rank())
